744692c95d358a2362bfc22ff470f2c950039cbb
Mercedes <mrcdssebastian@gmail.com>
2024-11-25 15:25:42 +0100

    solved lab with elena

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const logger = require('morgan')
-
+const cors = require("cors")
+const PORT = 5005
 //Instancia de la app
 
-const app = express()
 
+const app = express()
+app.use(
+    cors(
+        { origin: ['http://localhost:5005', 'http:/5005.com'] }
+    )
+)
 // Configuración
 
 app.use(logger('dev'))
@@ -12,20 +18,27 @@ app.use(express.json())
 
 // Creación de endpoints
 
-app.get('/api/cohorts', (req,res) => {
-    
+app.get('/api/cohorts', (req, res) => {
+
     const cohortsInfo = require('./cohorts.json')
 
     res.json(cohortsInfo)
 })
 
-app.get('/api/students', (req,res) => {
-    
+app.get('/api/students', (req, res) => {
+
     const studentsInfo = require('./students.json')
 
     res.json(studentsInfo)
 })
 
+
+app.get('/*', (req, res) => {
+
+
+
+    res.sendStatus(404)
+})
 // Levantamos el server
 
-app.listen (5005, () => console.log('Server running on port 5005'))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
